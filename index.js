cdf65e5f351b41a7f1706b8a4fd5544f852a1f52
Joseph Casiano <josephcasiano@gmail.com>
2017-12-04 03:51:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function BoardMember (name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
+  this.veto = veto(){
+    console.log = "No, I must disagree"
+  }
 }
-
-
